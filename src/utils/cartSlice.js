56c1003d68402fbc447e018c7241b1110522fe80
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-12-20 15:09:04 +0530

    added store

---
+++ src/utils/cartSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        items: []
+    },
+    reducers: {
+        addItem: (state, action) => {
+            state.items.push(action.payload);
+        },
+        removeItem: (state, action) => {
+            const itemToRemove = action.payload;
+            const indexToRemove = state.items.findIndex(item => item.id === itemToRemove.id);
+
+            if (indexToRemove !== -1) {
+                state.items.splice(indexToRemove, 1);
+            }
+        },
+        clearCart: (state) => {
+            state.items.length = 0;
+        }
+    }
+})
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions
+
+export default cartSlice.reducer;
