aeacda1a247f135c00b9647296c9669b2e3a8ae9
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-12-20 19:30:47 +0530

    login ui created

--- src/assets/styles/login.js
+++ src/assets/styles/login.js
@@ -2,5 +2,57 @@ import styled from "styled-components";
 
 export const LoginWrapper = styled.div`
     display: flex;
-    
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 60vh;
+    gap: 1rem;
+
+    .heading {
+        font-family: futura, sans-serif;
+        font-size: 2rem;
+    }
+
+    .username, .pass {
+        display: flex;
+        flex-direction: column;
+    }
+
+    label {
+        font-family: sans-serif;
+    }
+
+    input {
+        font-family: sans-serif;
+        padding: 0.5rem;
+        outline: none;
+        border: 1px solid #000;
+        border-radius: 5px;
+    }
+
+    .auth-btn, .down-sec {
+        font-family: sans-serif;
+        font-weight: 800;
+        text-decoration: underline;
+        cursor: pointer;
+    }
+
+    @media (max-width: 480px) {
+        .heading {
+            font-size: 1.5rem;
+        }
+
+        label {
+            font-size: 0.8rem;
+        }
+
+        input {
+            padding: 0.3rem;
+        }
+
+        .auth-btn, .down-sec {
+            font-size: 0.9rem;
+        }
+    }
 `
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,31 @@ import { LoginWrapper } from "../assets/styles/login";
 const Login = () => {
     return (
         <LoginWrapper>
-            
+            <div className="heading">
+                Register
+            </div>
+            <div className="username">
+                <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    placeholder="Enter your username"
+                    id="username"
+                />
+            </div>
+            <div className="pass">
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    placeholder="Enter your password"
+                    id="password"
+                />
+            </div>
+            <div className="auth-btn">
+                Create
+            </div>
+            <div className="down-sec">
+                Login instead
+            </div>
         </LoginWrapper>
     )
 }
