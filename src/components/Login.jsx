cfd1bdb00decaddc25f61be6aa2ab454c9f4f749
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-12-20 22:06:20 +0530

    register req

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,49 @@
 import { LoginWrapper } from "../assets/styles/login";
+import { useState } from "react";
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleRegister = async () => {
+        try {
+            const checkUserExist = await fetch('http://localhost:8080/checkUser', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, password }),
+            });
+            if (checkUserExist.status === 404) {
+                try {
+                    const registerUser = await fetch('http://localhost:8080/register', {
+                        method: 'POST',
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({ username }),
+                    });
+                    if (registerUser.status === 201) {
+                        document.querySelector('.username input').value = '';
+                        document.querySelector('.password input').value = '';
+                        document.querySelector('.username input').placeholder = 'Registered succesfully';
+                    } else {
+                        console.log("Failed to register user");
+                    }
+                } catch (error) {
+                    console.error('Error registering user:', error);
+                }
+            }
+            else if (checkUserExist.status === 200) {
+                document.querySelector('.username input').value = '';
+                document.querySelector('.password input').value = '';
+                document.querySelector('.username input').placeholder = 'Username already exists';
+            }
+        } catch (error) {
+            console.error('Error checking user existence:', error);
+        }
+    };
+
     return (
         <LoginWrapper>
             <div className="heading">
@@ -12,6 +55,10 @@ const Login = () => {
                     type="text"
                     placeholder="Enter your username"
                     id="username"
+                    value={username}
+                    onChange={(e) => {
+                        setUsername(e.target.value);
+                    }}
                 />
             </div>
             <div className="pass">
@@ -20,9 +67,13 @@ const Login = () => {
                     type="password"
                     placeholder="Enter your password"
                     id="password"
+                    value={password}
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
                 />
             </div>
-            <div className="auth-btn">
+            <div className="auth-btn" onClick={handleValidation}>
                 Create
             </div>
             <div className="down-sec">
