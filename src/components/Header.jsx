3b1053cb45cc0a9f4b2fc1963d368675752929b1
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-12-20 11:30:47 +0530

    Header created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,40 @@
 import styled from "styled-components"
 
 const HeaderWrapper = styled.div`
-    padding: 1rem;
+    color: #213875;
+    border-bottom: 1px solid #dea1ea;
 `;
 
-const LeftSection = styled.div`
+const BaseDiv = styled.div`
+    height: 100%;
+    font-family: 'futura', sans-serif;
     div {
-        width: 20px;
-        height: 20px;
+        font-size: 1.3rem;
+        padding: 1rem;
+        height: 100%;
+    }
+`
+
+const LeftSection = styled(BaseDiv)`
+    div {
+        cursor: pointer;
+        border-right: 1px solid #dea1ea;
+    }
+
+    div:hover .underline{
+        width: 100%;
     }
 `;
 
 const Logo = styled.div`
-  
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 1.5rem;
 `;
 
-const RightSection = styled.div`
-    display: flex;
-
+const RightSection = styled(BaseDiv)`
     div {
-        width: 20px;
-        height: 20px;
+        cursor: pointer;
+        border-left: 1px solid #dea1ea;
     }
 `;
 
@@ -28,16 +42,23 @@ const Header = () => {
     return (
         <HeaderWrapper className="flex justify-between items-center">
             <LeftSection className="flex">
-                <div className="android"></div>
-                <div className="ios"></div>
+                <div className="android">
+                    Android 
+                </div>
+                <div className="ios">
+                    ios
+                </div>
             </LeftSection>
             <Logo>
                 Mobile-store
             </Logo>
             <RightSection className="flex">
-                <div className="login-btn"></div>
-                <div className="search-btn"></div>
-                <div className="cart-btn"></div>
+                <div className="login-btn">
+                    Login
+                </div>
+                <div className="cart-btn">
+                    Cart
+                </div>
             </RightSection>
         </HeaderWrapper>
     )
